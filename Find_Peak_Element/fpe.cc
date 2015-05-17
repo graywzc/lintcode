6e1e5cc4b6e20ca7b7d089ab6f596d906cd835bd
graywzc <graywzc@gmail.com>
2015-05-17 02:12:30 -0700

    0517

--- Find_Peak_Element/fpe.cc
+++ Find_Peak_Element/fpe.cc
@@ -6,7 +6,20 @@ public:
      */
     int findPeak(vector<int> A) {
         // write your code here
-        
+        assert(A.size()>=3); 
+        int l = 1;
+        int r = A.size()-2;
+        while(l<r)
+        {
+            int m = (l+r)/2; 
+            if(A[m]>A[m-1] && A[m]>A[m+1])
+                return m;
+            else if(A[m]<A[m-1])
+                r = m-1;
+            else 
+                l = m+1;
+        }
+        return l;
     }
 };
 
