fb3dff07984595cfdbd08fec129dec4bd45ce1f5
graywzc <graywzc@gmail.com>
2015-05-17 02:03:20 -0700

    0517

---
+++ Find_Peak_Element/fpe.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    int findPeak(vector<int> A) {
+        // write your code here
+        
+    }
+};
+
+
